c54e65ea7dda9d96dce74ad2ef4b3e98550ed51f
Prambudialief <prambualif@gmail.com>
2024-05-27 10:11:41 +0700

    fix: perubahan di index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $jmlGagal = mysqli_num_rows($gagalUjian);
 <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Dashboard</h1>
+                        <h1 class="mt-4">Home</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Home</li>
                         </ol>
